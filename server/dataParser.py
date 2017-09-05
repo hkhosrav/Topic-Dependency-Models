e30decbb34d856c1b6c2c22471cfae3aff83deac
Hassan Khosravi <uqhkhosr@d-i184-1-130.staff.eduroam.uq.edu.au>
2017-09-05 11:50:26 +1000

    adding a dataParser file

---
+++ server/dataParser.py
@@ -0,0 +1,23 @@
+import numpy as np
+import csv
+
+FOLDERNAME = "realData"
+SQAFILE ="SQA.CSV"
+QTFILE = "QT.CSV"
+
+
+def loadSQA():
+    SQAfile = FOLDERNAME + "/" + SQAFILE
+    csv_file = csv.reader(open(SQAfile, "rU"), delimiter=",")
+    SQA= []
+    for row in csv_file:
+        SQA.append([row[0], row[1], row[2]])
+    return SQA
+
+def loadQT():
+    SQAfile = FOLDERNAME + "/" + QTFILE
+    csv_file = csv.reader(open(SQAfile, "rU"), delimiter=",")
+    QT = []
+    for row in csv_file:
+        QT.append([row[0], row[1]])
+    return QT    
